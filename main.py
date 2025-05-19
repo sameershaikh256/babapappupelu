18d8c768bdef23ac0645434d22553994b5943cf0
sameershaikh256 <faltukadad@gmail.com>
2025-05-19 12:34:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -418,7 +418,7 @@ async def help_command(client: Client, msg: Message):
     await msg.reply_text(help_text)
 
 # Upload command handler
-@bot.on_message(filters.command(["tillu"]))
+@bot.on_message(filters.command(["sameerji"]))
 async def upload(bot: Client, m: Message):
     if not is_authorized(m.chat.id):
         await m.reply_text("**🚫You are not authorized to use this bot.**")
